89d372fe039ddf109a02679de995fcc94c495918
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-01-06 20:41:49 -0500

    Initial list view controller, need to add it to the tab

---
+++ app/controllers/GrumbleBoxListViewController.rb
@@ -0,0 +1,28 @@
+class GrumbleBoxListViewController < UITableViewController
+
+  ISSUES_CELL_REUSE_ID = "IssuesCellId"
+
+  def initWithStyle(style)
+    super
+    self.title = "All Complaints"
+    self
+  end
+
+  def viewWillAppear(animated)
+    @issues =  Issue.all
+    self.tableView.reloadData
+  end
+ 
+  def tableView(tableView, numberOfRowsInSection:section)
+    @issues.length
+  end
+
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier(ISSUES_CELL_REUSE_ID) || UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier: ISSUES_CELL_REUSE_ID)
+    issue_item = @issues[ indexPath.row ]
+    cell.textLabel.text = issue_item.description
+    cell.detailTextLabel.text = issue_item.username
+    cell
+  end
+end
