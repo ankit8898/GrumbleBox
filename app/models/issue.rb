e0b08a979e96e106c8409f506b166c4e17d6d567
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-01-06 20:00:54 -0500

    Code refactoring

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,11 +1,10 @@
 class Issue < NanoStore::Model  
   # Do not move this method down
-  def self.initialize_attributes(*attrs)
-    attrs.each{|attr| attribute attr}
+  def self.attributes(*attrs)
+    attrs.size > 0 ? attrs.each{|attr| attribute attr} : super
   end
 
-  initialize_attributes :description, :username, :address, :created_at
-  
+  attributes :description, :username, :address, :created_at
    
   class << self  
     def create_new(username, description, address) 
